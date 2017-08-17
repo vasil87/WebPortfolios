473a75812ac18d54c3951a8c38eb376304228fb4
Peter Georgiev <peter_85@abv.bg>
2017-08-17 22:42:28 +0300

    working on HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    [MdButtonModule, MdCheckboxModule],
   ],
   providers: [],
   bootstrap: [AppComponent]
