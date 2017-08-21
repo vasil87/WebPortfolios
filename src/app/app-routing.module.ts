4efe6fbb1ca47f555418acd1e169fc1cb5a8d4fb
Peter Georgiev <peter_85@abv.bg>
2017-08-21 23:10:46 +0300

    working on route /contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ContactComponent } from './shared/contact/contact.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/portfolios', pathMatch: 'full' },
-  { path: 'portfolios', loadChildren: './portfolios/portfolios.module#PortfoliosModule' }
+  { path: 'portfolios', loadChildren: './portfolios/portfolios.module#PortfoliosModule' },
+  { path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AsideComponent } from './aside/aside.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
     imports: [
         CommonModule,
         BrowserAnimationsModule,
         [MdButtonModule, MdCheckboxModule],
+        RouterModule,
     ],
     exports: [
         HeaderComponent,
@@ -21,7 +24,8 @@ import { AsideComponent } from './aside/aside.component';
     declarations: [
         HeaderComponent,
         FooterComponent,
-        AsideComponent
+        AsideComponent,
+        ContactComponent
 ],
     providers: [],
 })
