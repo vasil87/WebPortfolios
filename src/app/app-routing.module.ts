d82ec955c87304131402e68cecf364c65e9eb5ab
Peter Georgiev <peter_85@abv.bg>
2017-08-30 11:11:39 +0300

    creating my-portfolio-form module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/portfolios/all', pathMatch: 'full' },
   { path: 'portfolios', loadChildren: './portfolios/portfolios.module#PortfoliosModule' },
   { path: 'contact', component: ContactComponent},
+  { path: 'my-portfolio-form', loadChildren: './my-portfolio-form/my-portfolio-form.module#MyPortfolioFormModule'},
 ];
 
 @NgModule({
