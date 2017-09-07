fcd4b2ba22ceee41313b01e8aa55ff3c58bebefc
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-09-07 09:10:42 +0300

    static files

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 // Run the app by serving the static files
 // in the dist directory
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
 app.listen(process.env.PORT || 8080);
