a8b9a043b744d24d4c61958b77fcb6822f3ec347
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-09-07 09:22:24 +0300

    sdf

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + './dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
 // Run the app by serving the static files
 // in the dist directory
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + './dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
