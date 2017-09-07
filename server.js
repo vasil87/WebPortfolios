c0a94491f1ed6da210cb6ee2f7a7504a03407bf3
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-09-07 09:15:36 +0300

    server changes

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use('/js', express.static(__dirname + './dist'));
+app.use('/css', express.static(__dirname + './dist'));
+app.use('/img', express.static(__dirname + './dist/assets/images'));
+app.use('/ico', express.static(__dirname + './dist/assets/images'));
+
 // Start the app by listening on the default
 // Heroku port
 // Run the app by serving the static files
 // in the dist directory
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + './dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
